b47b7632b9191a3f99494200c6a194b8b251866c
mohamed22-code <mohamed19118044@gmail.com>
2026-07-21 14:16:29 +0300

    + Initialize project

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:evently_app/home/home_screen.dart';
+import 'package:evently_app/utils/app_routes.dart';
+import 'package:evently_app/utils/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      initialRoute: AppRoutes.homeRouteName,
+      routes: {
+        AppRoutes.homeRouteName: (context)=>HomeScreen(),
+      },
+    );
+  }
+}
+
